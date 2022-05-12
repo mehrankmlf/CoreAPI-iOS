d23d3231c8f750664b670c26a19285405ba09008
Mehran Kamalifard <mehran.kmlf@icloud.com>
2022-05-12 10:48:06 +0430

    Initial Commit

---
+++ CoreAPI-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreAPI-iOS
+//
+//  Created by Mehran on 2/22/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoreAPI-iOSUITests/CoreAPI_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CoreAPI_iOSUITestsLaunchTests.swift
+//  CoreAPI-iOSUITests
+//
+//  Created by Mehran on 2/22/1401 AP.
+//
+
+import XCTest
+
+class CoreAPI_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
