04f6f572420e6724cdee436322d3207f0fd500e2
Mehran <m.kamalifard@stsepehr.ir>
2022-05-15 11:09:57 +0430

    Added Activity Indicator

---
+++ CoreAPI-iOS/Classes/Base/BaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BaseViewController.swift
+//  CoreAPI-iOS
+//
+//  Created by Mehran Kamalifard on 5/15/22.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CoreAPI-iOS/Classes/Protocols/ViewModelBaseProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModelBaseProtocol.swift
+//  CoreAPI-iOS
+//
+//  Created by Mehran Kamalifard on 5/15/22.
+//
+
+import Foundation
